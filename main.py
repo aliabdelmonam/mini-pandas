2565ad8b0ccc226ae7ec7182507d273bd1582b63
Ali-Abdelmenam <aliabdelmonam37@gmail.com>
2025-12-18 15:07:19 +0200

    Upload to GithHub

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
+from dataframe import Dataframe
+from stats import Stats
 def main():
     # TODO: Read data
 
+    df = Dataframe.read_csv(data_path=r'D:\ITI\Python for ML\Lab\mini-project\starter_code\data\titanic.csv'
+                ,dtype_path=r"D:\ITI\Python for ML\Lab\mini-project\starter_code\data\titanic_dtype.csv")
+    
+    df = Dataframe(data=df.data, dtype=df.dtype)
 
+   
+    
+    df.describe()
     # TODO: Fill missing values
+    df.fillna(numeric_function='mean',cate_function='mode')
     # Numeric columns → mean
     # Categorical columns → mode
-
-
-    # TODO:Generate statistics file
+    # df.describe()
+    print(df.data)
 
 
     # TODO:Write cleaned data to CSV
 
-
+    df.to_csv(path='lol.csv')
 
 if __name__ == "__main__":
     main()
